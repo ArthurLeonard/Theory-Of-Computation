9d3196c070982b0b66e5f51dd2ffec34afa02815
Ning Yi <ialeonard@ucdavis.edu>
2019-08-01 21:17:32 -0700

    mvp

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -11,27 +11,33 @@ if __name__ == '__main__':
 filename = sys.argv[1]
 
 # TODO Read HTML file
-
+f = open("stackoverflow.html", "r")
+txt = f.read()
+#print(txt)
 
 # TODO Set up regex
-
+expression = r"https?:[\/]{2}.+\.[cn][eo][mt].*?"
 
 # TODO Find links using regex, save in list called 'matches'
-
-
+matches = re.findall(expression, txt)
+#print(a)
+f.close()
 # Check matches, print results
 # TODO Read in links from answers.txt (hint...this is a CSV file), 
+f = open("answers.txt", "r")
+answer_data = f.read()
+print('read answers', answer_data)
 # save in list called 'answer_data'
 
 
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
-#       break
-# print( result )
+result = "All links matched!"
+if len( matches ) != len( answer_data ):
+  result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
+else:
+  for i in range( len(answer_data) ):
+    if( matches[i] != answer_data[i] ):
+      result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
+      break
+print( result )
--- legacy/projects/state-mach-regex/phone/phone.py
+++ legacy/projects/state-mach-regex/phone/phone.py
@@ -4,18 +4,23 @@ import re # module for processing regular expressions https://docs.python.org/3/
 line = input("Enter a phone number to validate or 'exit' when done. ")
 
 # TODO Define your regex
-
+expression = r"\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}"
 
 while line != "exit":
     # TODO Find matches
-
+    match = re.findall(expression, line)
     
     # TODO If no match found, print that no number was found
-   
-   
+    if len(match) == 0:
+   		print("No valid phone number found")
+
     
     # TODO Else, break number up into area code, prefix, and suffic
-    
+    else:
+   		print("Area code:", match[0][0:3])
+   		print("Prefix:", match[0][3:6])
+   		print("Suffix:", match[0][6:10])
+
     
     # As a stretch goal, you can modify your regex to search for country codes
     # too and print that out as well!
